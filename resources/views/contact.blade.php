318d19879452cb896d1911281a13af8c0fd2f02a
Hien04 <nguyenhien041102@gmail.com>
2023-05-19 10:39:30 +0700

    upload file contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -30,7 +30,7 @@
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Contact Us</h5>
+                <h5 class="fw-bold text-primary text-uppercase">Contact UsS</h5>
                 <h1 class="mb-0">If You Have Any Query, Feel Free To Contact Us</h1>
             </div>
             <div class="row g-5 mb-5">
